77d5eae3d6926b6fda719283a888a7142e14aa0d
ColinHDev <colinheidfeld@gmail.com>
2021-12-07 21:35:18 +0100

    ResourceManager: removed redudant line breaks, PHPDocs and check if the plugin_data folder of the plugin exists

--- src/ColinHDev/AntiXRay/ResourceManager.php
+++ src/ColinHDev/AntiXRay/ResourceManager.php
@@ -6,7 +6,6 @@ use pocketmine\utils\Config;
 use pocketmine\utils\SingletonTrait;
 
 class ResourceManager {
-
     use SingletonTrait;
 
     private bool $antiXRayStandard;
@@ -14,26 +13,16 @@ class ResourceManager {
 
     public function __construct() {
         self::$instance = $this;
-
-        if (!is_dir(AntiXRay::getInstance()->getDataFolder())) mkdir(AntiXRay::getInstance()->getDataFolder());
-
         AntiXRay::getInstance()->saveResource("config.yml");
-
         $config = new Config(AntiXRay::getInstance()->getDataFolder() . "config.yml", Config::YAML);
         $this->antiXRayStandard = $config->get("antixray.standard", true);
         $this->worlds = $config->get("antixray.worlds", []);
     }
 
-    /**
-     * @return bool
-     */
     public function getAntiXRayStandard() : bool {
         return $this->antiXRayStandard;
     }
 
-    /**
-     * @return string[]
-     */
     public function getWorlds() : array {
         return $this->worlds;
     }

25ed38a4585d1aa8af5cfca1a0c6622209d9f503
ColinHDev <colinheidfeld@gmail.com>
2022-07-29 22:32:00 +0200

    AntiXRay: Made class use SingletonTrait

--- src/ColinHDev/AntiXRay/AntiXRay.php
+++ src/ColinHDev/AntiXRay/AntiXRay.php
@@ -5,17 +5,13 @@ namespace ColinHDev\AntiXRay;
 use ColinHDev\AntiXRay\listener\DataPacketSendListener;
 use ColinHDev\AntiXRay\listener\PlayerCreationListener;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\SingletonTrait;
 
 class AntiXRay extends PluginBase {
-
-    private static AntiXRay $instance;
-
-    public static function getInstance() : AntiXRay {
-        return self::$instance;
-    }
+    use SingletonTrait;
 
     public function onEnable() : void {
-        self::$instance = $this;
+        self::setInstance($this);
         ResourceManager::getInstance();
         $this->getServer()->getPluginManager()->registerEvents(new DataPacketSendListener(), $this);
         $this->getServer()->getPluginManager()->registerEvents(new PlayerCreationListener(), $this);
